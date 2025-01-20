bf546a68a6f6de3b12e168e7ecbd80991a6f1a21
Tadeo Fernandez <tadeo.v.fernandez@gmail.com>
2025-01-20 11:05:11 -0300

    agregamos funcion sortearAmigo

--- app.js
+++ app.js
@@ -2,9 +2,28 @@
 
 //declaracion de variables y constantes
 let amigos = [];
-const inputUsuario = document.getElementById("amigo");
 let nombreActual;
+const inputUsuario = document.getElementById("amigo");
 const listaAmigos = document.getElementById("listaAmigos");
+const amigoFinal = document.getElementById("resultado");
+
+//funcion para elejir y mostrar al amigo aleatorio
+function sortearAmigo(){
+
+    //utiliza un numero random basado en el array, borra la lista (puro estetico) y muestra al elejido
+    if (amigos.length > 0){
+
+        let indice = Math.floor(Math.random()*amigos.length) + 1;
+        let amigoElejido = amigos[indice];
+        listaAmigos.innerHTML = '';
+        amigoFinal.innerHTML = amigoElejido;
+
+    } else {
+
+        alert("No hay amigos para sortear");
+
+    }
+}
 
 //Funcion para mostrar la lista de amigos
 function actualizarLista(){
@@ -12,12 +31,13 @@ function actualizarLista(){
     listaAmigos.innerHTML = '';
 
     //este bucle crea un elemento lista, le asigna como contenido el amigo de la iteracion y luego lo agrega a la lista
-    for(let i = 0; i <= amigos.length; i++){
+    for(let i = 0; i < amigos.length; i++){
+
         const li = document.createElement('li');
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
-    }
 
+    }
 }
 
 //Funcion para agregar amigos a la lista al presionar el boton, de estar vacio envia alerta
