df9a27e49eeb519d622238614ca9ad53bd673985
Tadeo Fernandez <tadeo.v.fernandez@gmail.com>
2025-01-20 10:26:31 -0300

    agregue actualizarLista

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//declaracion de variables
+//declaracion de variables y constantes
 let amigos = [];
-let inputUsuario = document.getElementById("amigo");
+const inputUsuario = document.getElementById("amigo");
 let nombreActual;
+const listaAmigos = document.getElementById("listaAmigos");
 
-//Funcion para agregar amigos a la lista al presionar el boton
+//Funcion para mostrar la lista de amigos
+function actualizarLista(){
+
+    listaAmigos.innerHTML = '';
+
+    //este bucle crea un elemento lista, le asigna como contenido el amigo de la iteracion y luego lo agrega a la lista
+    for(let i = 0; i <= amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+
+}
+
+//Funcion para agregar amigos a la lista al presionar el boton, de estar vacio envia alerta
 function agregarAmigo(){
 
     nombreActual = inputUsuario.value;
@@ -16,10 +31,11 @@ function agregarAmigo(){
         amigos.push(nombreActual);
         inputUsuario.value = '';
         console.log(amigos);
+        actualizarLista();
 
     } else{
-        
+
         alert("Por favor, inserte un nombre");
-        inputUsuario.innerHTML = '';
+
     }
 }
