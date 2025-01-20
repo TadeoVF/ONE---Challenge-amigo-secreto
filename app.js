a79b385db0d21d5f09b52ee61bb020322ef2cbe9
Tadeo Fernandez <tadeo.v.fernandez@gmail.com>
2025-01-20 09:27:15 -0300

    primer commit, agregamos funcion de añadir

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//declaracion de variables
+let amigos = [];
+let inputUsuario = document.getElementById("amigo");
+let nombreActual;
+
+//Funcion para agregar amigos a la lista al presionar el boton
+function agregarAmigo(){
+
+    nombreActual = inputUsuario.value;
+    console.log(nombreActual);
+
+    if (nombreActual != ''){
+
+        amigos.push(nombreActual);
+        inputUsuario.value = '';
+        console.log(amigos);
+
+    } else{
+        
+        alert("Por favor, inserte un nombre");
+        inputUsuario.innerHTML = '';
+    }
+}
